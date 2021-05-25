f41b4e78214c656644bc998678e3589cd2bdd387
Talha Noyon <talha.noyon10@gmail.com>
2021-05-25 06:34:55 +0600

    main branch push

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 const route = require('express').Router();
-
+const path = require('path');
+route.get("*", (req, res) => {
+  console.log("baaal");
+  if (req.url.indexOf('/dashboard') > -1) {
+    res.redirect('/');
+  } else {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  }
+  
+});
 route.use('/folder',require('./api/Folder'));
 
 module.exports = route;
